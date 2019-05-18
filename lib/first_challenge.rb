47ff689c75211b3bc63d67fdc1cbb98acb5771a3
mcumm64 <mcumm64@gmail.com>
2019-05-18 23:04:04 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
-   epic_tragedy = {
-    :montague => [
+   epic_tragedy = [
+    :montague => {
       :patriarch => {:name => "Lord Montague", :age => "53"},
       
       :matriarch => {:name => "Lady Montague", :age => "54"},
